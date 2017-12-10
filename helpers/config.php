c67042b6c971efb3d5c01c07d43fba232a2780e8
Vinicius Moura <viniciusmouraesilva2204@outlook.com>
2017-12-10 17:10:47 -0200

    ajeitando charset

--- helpers/config.php
+++ helpers/config.php
@@ -1,5 +1,5 @@
 <?php
 #configurações banco PDO
-define("DSN","mysql:dbname=loja;host=localhost;utf8mb4");
+define("DSN","mysql:dbname=loja;host=localhost;charset=utf8mb4");
 define("USUARIO","root");
 define("SENHA","@Zuruca052");
